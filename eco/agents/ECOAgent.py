2ccbbd37502a7ee09a3b76663d9d04c9c35d2209
RidiculousBuffal <3322946829@qq.com>
2026-01-07 20:33:11 +0800

    finish ecoagent

--- eco/agents/ECOAgent.py
+++ eco/agents/ECOAgent.py
@@ -1,13 +1,15 @@
 import asyncio
 
 from langchain.agents import create_agent
+from langchain_core.messages import HumanMessage
 
+from eco.agents.BaseAgent import BaseAgent
 from eco.agents.ROIRetriever import ROIRetriever
 from eco.config import Config
 from eco.joern.JoernDetector import JoernAdvisor
 
 
-class ECOAgent:
+class ECOAgent(BaseAgent):
     def __init__(self):
         self.agent = create_agent(Config.chatOpenAI)
         self.joern_detector = JoernAdvisor()
@@ -21,12 +23,20 @@ class ECOAgent:
     async def build_prompt(self, input_code):
         joern_task = self.joern_detector.analyze_code(input_code)
         retrieve_task = self._retrieve_task(input_code)
-        joern_res,retrieve_res = await asyncio.gather(joern_task, retrieve_task)
-        prompt = (f"""
-Given a program and optimization tips, optimize the program and provide a more efficient version.
-### Original code:
-{input_code}\n"""
-+f"""### BottleNecks [detected by joern]:\n"""
-+'\n'.join(joern_res)
-+f"""### Performance-related characteristics \n""")
+        joern_res, retrieve_res = await asyncio.gather(joern_task, retrieve_task)
+        prompt = (
+                    f"""Given a program and optimization tips, optimize the program and provide a more efficient version.\n### Original code:\n{input_code}\n"""
+                    + f"""### BottleNecks [detected by joern]:\n"""
+                    + '\n'.join(joern_res)
+                    + f"""### Performance-related characteristics \n""")
+        for i in range(0, len(retrieve_res)):
+            prompt += f"======================={i}:=======================\n"
+            prompt += f"slow_code:\n" + retrieve_res[i].metadata.get("metadata", {}).get("src_code", "") + '\n'
+            prompt += f"fast_code:\n" + retrieve_res[i].metadata.get("metadata", {}).get("tgt_code", "") + '\n'
+            prompt += f"runtime optimization instructions:\n" + retrieve_res[i].page_content + '\n'
+        return prompt
+
+    async def call(self, input_code):
+        human_message = HumanMessage(content=await self.build_prompt(input_code))
+        await self.agent.ainvoke(self.buildMessage([human_message]))
 
